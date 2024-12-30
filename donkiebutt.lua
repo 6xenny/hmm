f6c4f2bbd3808cc38f8160d12aa9042dfa352304
6xenny <6xeshi@gmail.com>
2024-12-29 18:12:40 -0600

    Update donkiebutt.lua

--- donkiebutt.lua
+++ donkiebutt.lua
@@ -1,7 +1,7 @@
 local UpdateList = {
   Crim = {
     ["2024-12-29T14:23:52.3676057Z"] = true, -- Standard
-    ["2024-12-29T14:23:52.3676057Z"] = true, -- Casual
+    ["2024-12-23T21:38:05.58Z"] = true, -- Casual
     ["2024-12-29T14:23:52.3676057Z"] = true, -- Infection
     ["2024-12-29T14:23:52.3676057Z"] = true -- Brawl
   }

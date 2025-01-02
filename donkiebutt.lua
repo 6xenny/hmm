926c271bcc425fca1bbbaf9c2b0d5cd222244927
6xenny <6xeshi@gmail.com>
2025-01-01 23:10:17 -0600

    Update donkiebutt.lua

--- donkiebutt.lua
+++ donkiebutt.lua
@@ -4,6 +4,9 @@ local UpdateList = {
     ["2024-12-23T21:38:05.58Z"] = true, -- Casual
     ["2024-12-23T12:11:59.477Z"] = true, -- Infection
     ["2024-12-23T12:11:35.64Z"] = true -- Brawl
+  },
+  Planks = {
+    [2025-01-02T03:58:20.176346Z] = true
   }
 }
 return UpdateList

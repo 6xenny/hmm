a96578683a0edbb01ff0cbf0591212bb4c0af27a
6xenny <6xeshi@gmail.com>
2025-02-24 21:18:28 -0600

    Create Hit Sounds Config.lua

---
+++ Hit Sounds Config.lua
@@ -0,0 +1,10 @@
+local Sounds = {
+	["Rust.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/Rust.wav",
+	["skeet.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/skeet.wav",
+	["neverlose.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/neverlose.wav",
+	["fatality.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/fatality.wav",
+	["phonk.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/phonk.wav",
+	["bonk.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/bonk.wav",
+	["pluh.wav"] = "https://github.com/6xenny/hmm/raw/refs/heads/main/Hit%20Sounds/pluh.wav",
+}
+return Sounds
